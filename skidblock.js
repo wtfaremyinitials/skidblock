126a0f8c8d0df73f5511479227711f9b143158ef
Will Franzen <wtfranzen@gmail.com>
2014-01-12 21:26:56 -0600

    Wait until the page is loaded before setInterval

--- skidblock.js
+++ skidblock.js
@@ -1,7 +1,11 @@
-window.setInterval(function(){
-	if(document.body.contentEditable == 'true') {
-		document.body.innerHTML = "<h1>Go away scriptkiddie</h1>";
-		document.body.contentEditable = false;
-		document.designMode = 'off';
-	}
-}, 500);
+window.onload = function() {
+		
+	window.setInterval(function(){
+		if(document.body.contentEditable == 'true') {
+			document.body.innerHTML = "<h1>Go away scriptkiddie</h1>";
+			document.body.contentEditable = false;
+			document.designMode = 'off';
+		}
+	}, 500);
+		
+};
