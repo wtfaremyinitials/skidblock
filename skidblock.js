884363de89e74512bc3275927b52ef42f3341b39
Will Franzen <wtfranzen@gmail.com>
2014-01-21 09:35:37 -0600

    Add window.skidblock object

--- skidblock.js
+++ skidblock.js
@@ -1,11 +1,15 @@
 window.onload = function() {
+	
+	window.skidblock = {};
+	window.skidblock.version = "1.1.0";
+	window.skidblock.callback = function(){};
 		
 	window.setInterval(function(){
 		if(document.body.contentEditable == 'true') {
 			document.body.contentEditable = false;
 			document.designMode = 'off';
-			if(window.skidblockcallback) {
-				window.skidblockcallback();
+			if(window.skidblock.callback) {
+				window.skidblock.callback();
 			}
 		}
 	}, 500);
